dcc02cae969d7120613d327d3efc2b9bfddf2077
Max Pfeiffer <max@maxpfeiffer.ch>
2023-05-21 23:02:06 +0200

    Added tests for publising the image
    I had to restructure the whole test setup a bit to make it work

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,13 @@ from random import randrange
 
 import docker
 import pytest
-from docker.models.images import Image
+from docker.client import DockerClient
 from semver import VersionInfo
-
-from build.constants import (
-    TARGET_ARCHITECTURES,
-)
-from build.images import PythonPoetryImage
 
 
 @pytest.fixture(scope="session")
-def docker_client() -> docker.client:
-    return docker.client.from_env()
+def docker_client() -> DockerClient:
+    return docker.from_env()
 
 
 @pytest.fixture(scope="session")
@@ -23,24 +18,3 @@ def version() -> str:
     )
     version_string: str = str(version)
     return version_string
-
-
-@pytest.fixture(scope="session", params=TARGET_ARCHITECTURES)
-def python_poetry_image(docker_client, version, request) -> str:
-    target_architecture: str = request.param
-
-    python_poetry_image: Image = PythonPoetryImage(docker_client).build(
-        target_architecture, version=version
-    )
-    image_tag: str = python_poetry_image.tags[0]
-    yield image_tag
-    docker_client.images.remove(image_tag, force=True)
-
-
-@pytest.fixture(scope="function")
-def cleaned_up_test_container(docker_client, request) -> None:
-    test_container_name: str = request.param
-    yield test_container_name
-    test_container = docker_client.containers.get(test_container_name)
-    test_container.stop()
-    test_container.remove()
