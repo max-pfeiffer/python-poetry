1747e7b0ddac02e61ee8f789285d078d7c9b7433
Max Pfeiffer <max@maxpfeiffer.ch>
2023-05-21 23:30:33 +0200

    Fixed issue in build tests, updated pre commit config

--- tests/build_image/conftest.py
+++ tests/build_image/conftest.py
@@ -12,9 +12,7 @@ def python_poetry_image(
 ) -> str:
     target_architecture: str = request.param
 
-    python_poetry_image: Image = PythonPoetryImage(docker_client).build(
-        target_architecture, version=version
-    )
+    python_poetry_image: Image = PythonPoetryImage(docker_client, target_architecture, version).build()
     image_tag: str = python_poetry_image.tags[0]
     yield image_tag
     docker_client.images.remove(image_tag, force=True)
