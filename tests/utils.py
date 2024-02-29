7503f3ca224206d5c986c56666354dc956204976
Max Pfeiffer <max@maxpfeiffer.ch>
2024-02-29 18:15:55 +0100

    Simplified ImageTagComponents class

--- tests/utils.py
+++ tests/utils.py
@@ -11,9 +11,9 @@ class ImageTagComponents:
     image_name: str
     tag: str
     version: str
-    target_architecture: str
     poetry_version: str
     python_version: str
+    os_variant: str
 
     @classmethod
     def create_from_reference(cls, tag: str):
@@ -23,31 +23,16 @@ class ImageTagComponents:
         tag: str = ref["tag"]
 
         tag_parts: list[str] = tag.split("-")
-        target_architecture_index = [
-            index
-            for index, tag_part in enumerate(tag_parts)
-            if tag_part.startswith("poetry")
-        ][0]
-        python_version_index = [
-            index
-            for index, tag_part in enumerate(tag_parts)
-            if tag_part.startswith("python")
-        ][0]
-
-        version: str = "-".join(tag_parts[:target_architecture_index])
-        target_architecture: str = "-".join(
-            tag_parts[target_architecture_index:]
-        )
-        poetry_version: str = tag_parts[target_architecture_index].lstrip(
-            "poetry"
-        )
-        python_version: str = tag_parts[python_version_index].lstrip("python")
+        version: str = tag_parts[0]
+        poetry_version: str = tag_parts[1].lstrip("poetry")
+        python_version: str = tag_parts[2].lstrip("python")
+        os_variant: str = "-".join(tag_parts[2:])
         return cls(
             registry=registry,
             image_name=image_name,
             tag=tag,
             version=version,
-            target_architecture=target_architecture,
             poetry_version=poetry_version,
             python_version=python_version,
+            os_variant=os_variant,
         )
